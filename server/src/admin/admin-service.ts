c61eafa0a65c9b105e8545bbd0c6279ca5766588
Antoine Lebel <antoleb@hotmail.fr>
2019-03-15 19:52:42 +0100

    Add expiration for admin token

--- server/src/admin/admin-service.ts
+++ server/src/admin/admin-service.ts
@@ -6,7 +6,7 @@ const challengeDao = require('../challenge/challenge-dao');
 
 export const authenticate = (username: string, password: string) => {
     if (config.admin.username === username && config.admin.password === password) {
-        return {token: jwt.sign({ sub: '000', username: username, isAdmin: true }, config.adminSecret)};
+        return {token: jwt.sign({ sub: '000', username: username, isAdmin: true }, config.adminSecret, {expiresIn: '5h'})};
     }
     return;
 };
