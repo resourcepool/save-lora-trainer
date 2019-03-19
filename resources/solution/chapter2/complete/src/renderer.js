32b0835deb0b455e9a8406c03888db1bd796ce9f
f2guibert <f2guibert@gmail.com>
2019-03-19 10:06:38 +0100

    homogeneiblabla the controller front

--- resources/solution/chapter2/complete/src/renderer.js
+++ resources/solution/chapter2/complete/src/renderer.js
@@ -32,4 +32,4 @@ const fireCustomCmd = () => {
 document.getElementById("connect").addEventListener("click", wisnodeService.initConnect);
 document.getElementById("send_location").addEventListener("click", rescueService.sendGpsLocation);
 document.getElementById("fire-custom-cmd").addEventListener("click", fireCustomCmd);
-document.getElementById("logo_takima").addEventListener("click", wisnodeService.debug);
+document.getElementById("logo-takima").addEventListener("click", wisnodeService.debug);
